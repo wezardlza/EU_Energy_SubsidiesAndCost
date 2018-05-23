c87f0448bd0ce79701ca1559ff4d2fc51730d025
wezardlza <584121079@qq.com>
2018-05-23 23:38:48 +0100

    Test_on_Physical_Quantity.cpp works now

--- Test_on_Physical_Quantity.cpp
+++ Test_on_Physical_Quantity.cpp
@@ -1,25 +1,17 @@
+template <class Type>
 class Physical_Quantity
 {
 public:
 	//Note that the length of u is variable; u reprensents the unit of the physical value the C-type string
-	template<size_t N> Physical_Quantity(double, const char(&)[N]);
+	Physical_Quantity(Type q, const char *u) : quantity(q), unit_name(u){}
 
 private:
-	double quantity;
-	size_t unit_name_length;
-	char * unit_name;
+	Type quantity;
+	const char * unit_name;
 };
 
-int main() {
-	double v = 1.0;
+int main(){
+	int v = 1;
 	const char *x = "Hello";
-	Physical_Quantity a(v, x);
-}
-
-template<size_t N>
-Physical_Quantity::Physical_Quantity(double q, const char(& u)[N])
-{
-	Physical_Quantity<Type>::quantity = q;
-	Physical_Quantity<Type>::unit_name = u;
-	Physical_Quantity<Type>::unit_name_length = N;
+	Physical_Quantity<int> a(v, x);
 }
