23a79429672e0febe5b26edaff271b5b91b13436
wezardlza <584121079@qq.com>
2018-05-24 09:22:27 +0100

    Add header guard

--- Test_on_Physical_Quantity.cpp
+++ Test_on_Physical_Quantity.cpp
@@ -1,17 +1,20 @@
+#pragma once
+#ifndef PHYSICAL_QUANTITY
 template <class Type>
 class Physical_Quantity
 {
 public:
 	//Note that the length of u is variable; u reprensents the unit of the physical value the C-type string
-	Physical_Quantity(Type q, const char *u) : quantity(q), unit_name(u){}
+	Physical_Quantity(const Type q, const char *u) : value(q), unit_name(u) {}
 
 private:
-	Type quantity;
+	const Type value;
 	const char * unit_name;
 };
+#endif // !PHYSICAL_QUANTITY
 
 int main(){
-	int v = 1;
+	const int v = 1;
 	const char *x = "Hello";
-	Physical_Quantity<int> a(v, x);
+	Physical_Quantity<int> a(v,x);
 }
