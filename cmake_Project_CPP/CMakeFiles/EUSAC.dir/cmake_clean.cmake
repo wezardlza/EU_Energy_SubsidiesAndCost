4fe72edb91c715f4b1971aecb3fa8f0f1dbbb57d
Ziang Li <zli@localhost.localdomain>
2018-07-19 22:12:52 +0100

    Updated.

---
+++ cmake_Project_CPP/CMakeFiles/EUSAC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EUSAC.dir/home/zli/Dropbox/Ziang_Proj/CPP_Proj/EU_Energy_SubsidiesAndCost/Source_Files/ZAMATH.cpp.o"
+  "CMakeFiles/EUSAC.dir/home/zli/Dropbox/Ziang_Proj/CPP_Proj/EU_Energy_SubsidiesAndCost/Source_Files/SRF.cpp.o"
+  "CMakeFiles/EUSAC.dir/home/zli/Dropbox/Ziang_Proj/CPP_Proj/EU_Energy_SubsidiesAndCost/Source_Files/EUSAC.cpp.o"
+  "CMakeFiles/EUSAC.dir/home/zli/Dropbox/Ziang_Proj/CPP_Proj/EU_Energy_SubsidiesAndCost/Source_Files/Test_Main_Script.cpp.o"
+  "EUSAC.pdb"
+  "EUSAC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EUSAC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
